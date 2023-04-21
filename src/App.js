db98a754ebc88c9970763c21042654a7255d01b4
Hwi Jeong Park <heee2x1@gmail.com>
2023-04-21 21:52:08 +0900

    feat: history, getGeo / refectoring

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import backgroundImage from './assets/sunset.jpeg';
 
 const Hello = styled.div`
   &:before{
-    background : url(${ (props) => props.image });
-    background-color: ${ (props) => props.color };
+    background-color : ${props => props.color || '#FFFFFF'};
+    background-image : url(${ props => props.image });
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
@@ -50,51 +50,47 @@ const StartPage = styled.div`
 
 function App(props) {
 
-  const [data,setData] = useState({})
+  const [weatherData,setWeatherData] = useState(null)
+  const [data,setData] = useState(null)
+  const [history,setHistory] = useState([])
   const [location, setLocation] = useState('')
-  const [color,setlazyColor] = useState('')
-  const [image,setImages] = useState(backgroundImage)
-  const [history,setHistory] = useState([]);
-  const [historyImg,setHistoryImg] = useState([]);
-  
+
   const FREEWEATHER_API_KEY = process.env.REACT_APP_FREEWEATHER_API_KEY;
   const PEXEL_API_KEY = process.env.REACT_APP_PEXEL_API_KEY;
 
   const weatherUrl = `
   https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${FREEWEATHER_API_KEY}
   `
+  
 
-  const fetchImages = async (keyword) => {
-    try {
-      // Pexels API 호출 URL
-      const url = "https://api.pexels.com/v1/search";
-      console.log(keyword);
-      // Pexels API 호출 매개변수
-      const params = {
-        "query": keyword,
-        "per_page": 1,
-        "page": 1,
-      };
+  const getLocation  = () => {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }else{
+      console.log( "no" );
+    }
+  }
+  const showPosition = (position) => {
+    let userLatitude = position.coords.latitude
+    let userLongitude = position.coords.longitude
+    const weatherPositionUrl = `
+      https://api.openweathermap.org/data/2.5/weather?lat=${userLatitude}&lon=${userLongitude}&units=metric&appid=${FREEWEATHER_API_KEY}
+    `
+    axios.get(weatherPositionUrl).then((response)=>{
+      setWeatherData(response.data);
+    })
+  }
 
-      // Pexels API 호출
-      const response = await axios.get(url, {
-        params,
-        headers: {
-          Authorization: PEXEL_API_KEY,
-        }
-      });
-      // Pexels API 응답 데이터 파싱
-      const data = response.data.photos[0];
+  useEffect(()=>{
+    getLocation();
+  },[])
 
-      // 검색 결과 이미지 URL 추출
-
-      setImages(data.src.original);
-      setHistoryImg(  historyImg => [...historyImg, data.src ])
-      // console.log(historyImg);
-      
-
-    } catch (error) {
-      console.log(error);
+  const searchLocation = (event) => {
+    if ( event.key === 'Enter'){
+      axios.get(weatherUrl).then((response)=>{
+        setWeatherData(response.data);
+      })
+      setLocation('')
     }
   }
 
@@ -102,109 +98,132 @@ function App(props) {
     ['en'], {type: 'region'}
   );
 
-  const searchLocation = (event) => {
-    if ( event.key === 'Enter'){
-      axios.get(weatherUrl).then((response)=>{
-        const countryName = regionNames.of(response.data.sys.country);
-        const weatherDesc = `${countryName} ${response.data.name} ${response.data.weather[0].description} street`;
-        fetchImages(weatherDesc).then((temp01) => {
-          setData(response.data);
-        });
+  useEffect(() => {
+    if (weatherData !== null) {
+      const countryName = regionNames.of(weatherData.sys.country);
+      const weatherDesc = `${countryName} ${weatherData.name} ${weatherData.weather[0].description}`;
+      fetchImages(weatherDesc).then((imageResult) => {
+        const Data = {
+          name: weatherData.name,
+          temp: weatherData.main.temp,
+          weather: weatherData.weather[0].main,
+          feels: weatherData.main.feels_like,
+          humidity: weatherData.main.humidity,
+          wind: weatherData.wind.speed,
+          img: {
+            original: imageResult.original,
+            small: imageResult.small,
+            color: imageResult.color
+          }
+        };
+        setData(Data);
+        setHistory([...history, Data]);
+      });
+    }
+  }, [weatherData]);
 
-        setHistory([...history, response.data]);
-      })
-      setLocation('')
+  async function fetchImages(keyword) {
+    try {
+      // Pexels API 호출 URL
+      const url = "https://api.pexels.com/v1/search";
+      console.log(keyword);
+      // Pexels API 호출 매개변수
+      const params = {
+        query: keyword,
+        per_page: 1,
+        page: 1,
+      };
+      // Pexels API 호출
+      const response = await axios.get(url, {
+        params,
+        headers: {
+          Authorization: PEXEL_API_KEY,
+        },
+      });
+      // Pexels API 응답 데이터 파싱
+      const data = response.data.photos[0];
+      // 검색 결과 이미지 URL 추출
+      const images = {
+        original: data.src.original,
+        small: data.src.small,
+        color: data.avg_color
+      };
+
+      return images;
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  
-
   const backToHistory = (targetData,key)=>{
-    setHistory([...history,history[key]]);
-    console.log("setdata:"+targetData.name);
-    console.log("sethistory:"+data.name);
-    console.log(history);
     setData(targetData);
-    setHistoryImg( historyImg => [...historyImg, historyImg[key] ])
-    setImages(historyImg[key].original);
-
-    // console.log(key);
-    // console.log(targetData);
-    // console.log(historyImg[key]);
-    // console.log(historyImg);
-    // 
   }
 
-  // useEffect(()=>{
-  //   if( data.main !== undefined ){
-      
-  //   }
-
-  // },[data])
 
   return (
-    <Hello className="app" image={image} color={color}>
+    <Hello className="app" image={ data ? data.img.original : backgroundImage } color={data ? data.img.color : "#ccc" } >
       <div className="search">
         <input type="text" value={location} onChange={event => setLocation(event.target.value)} placeholder='Enter Location' onKeyPress={searchLocation}/>
       </div>
-      {data.name === undefined &&
+      {data === null &&
         <StartPage>
           <p>
             enter Location
           </p>
         </StartPage>
       }
-      {data.name !== undefined &&
+      {data !== null &&
       <div className="container">
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1>: null}
+            <h1>{data.temp.toFixed()}°C</h1>
           </div>
           <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p>: null}
+            <p>{data.weather}</p>
           </div>
         </div>
           <div>
             <div className="bottom">
               <div className="feels">
-                {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p>: null}
+                <p className="bold">{data.feels.toFixed()}°C</p>
                 <p>Flells Like</p>
               </div>
               <div className="humidity">
-                {data.main ? <p className="bold">{data.main.humidity}%</p>: null}
+                <p className="bold">{data.humidity}%</p>
                 <p>humidity</p>
               </div>
               <div className="wind">
-                {data.wind ? <p className="bold">{data.wind.speed}MPH</p>: null}
+                <p className="bold">{data.speed}MPH</p>
                 <p>Wind Speed</p>
               </div>
             </div>
             <div className="history">
               {/* <p>History</p> */}
               <div className="history-wrapper">
-                {history.map( function(value,key){
-                  if(key !== history.length -1){
-                    const result = <HidtoryItme historyImg={historyImg[key].small} key={key} onClick={() => backToHistory(value,key)}>
-                        <div>
-                          <p>
-                            {value.name}
-                          </p>
-                          <p>
-                            {value.main.temp.toFixed()}°C
-                          </p>
-                        </div>
-                        <div>
-                          <p>
-                            {data.weather[0].main}
-                          </p>
-                        </div>
-                      </HidtoryItme>
-                    return result
-                  }
-                })}
+                {history === null ? (
+                  <p>Array is null</p>
+                ) : (
+                  history.map((value,key) => (
+                    <HidtoryItme key={key} historyImg={value.img.small} onClick={() => backToHistory(value,key)} >
+                      <div>
+                        <p>
+                          {value.name}
+                        </p>
+                        <p>
+                          {value.temp.toFixed()}°C
+                        </p>
+                      </div>
+                      <div>
+                        <p>
+                          {value.weather}
+                        </p>
+                      </div>
+                    </HidtoryItme>
+                  ))
+                )}
                 </div>
             </div>
           </div>
